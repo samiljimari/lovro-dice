c8a27821f41e6e40d2b033b3a249ea2b955edd58
Sami Ljimari <48930092+samiljimari@users.noreply.github.com>
2021-04-15 14:30:07 +0200

    added comments

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
 'use strict';
 
-//const gumb = document.getElementsByClassName('test');
-document.addEventListener('click',function(e){
-    if(e.target.name == "gumb") {
-        let randbroj = Math.trunc(Math.random()*6)+1;
-        let slika = document.getElementById('slikica');
-        slika.src = "dice-" + randbroj + ".png";
+//const gumb = document.getElementsByClassName('test'); // not a good way to define button
+document.addEventListener('click',function(e){ // this method to find button click is more reliable and less resource intensive / faster
+    if(e.target.name == "gumb") { 
+        let randbroj = Math.trunc(Math.random()*6)+1; // needs to trigger everytime the button gets clicked or it will have initial value all the time
+        let slika = document.getElementById('slikica'); // needs to get re-instated on every click or the scope can't reach the declaration
+        slika.src = "dice-" + randbroj + ".png"; // idk if this had to be changed but this is how I connect dynamic image names :D
     }
 })
